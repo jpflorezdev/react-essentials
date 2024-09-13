0679929173f059c039d6411138b4e54fb7345afc
Juan Pablo Florez Castañeda <juanpabloflorezcastaneda@Juans-MacBook-Pro.local>
2024-09-13 11:24:33 -0500

    CoreConcepts sections abstracted in to a component

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,9 +1,8 @@
-import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
-import CoreConcept from "./components/CoreConcepts/CoreConcept.jsx";
 import TabButton from "./components/TabButton/TabButton.jsx";
 import { useState } from "react";
 import { EXAMPLES } from "./data.js";
+import CoreConcepts from "./components/CoreConcept/CoreConcepts.jsx";
 
 function App() {
   const [selectedTopic, setSelectedTopic] = useState();
@@ -28,14 +27,7 @@ function App() {
     <div>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((coreConcept) => (
-              <CoreConcept key={coreConcept.title} {...coreConcept} />
-            ))}
-          </ul>
-        </section>
+        <CoreConcepts />
         <section id="examples">
           <h2>Examples</h2>
           <menu>
